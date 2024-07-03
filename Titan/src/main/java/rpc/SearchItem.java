f207d141ce6ee9c5594863dd52d776c3cbfe8131
Ben Li <109922463+bli202@users.noreply.github.com>
2024-07-03 13:47:47 -0700

    Added Servlets

--- Titan/src/main/java/rpc/SearchItem.java
+++ Titan/src/main/java/rpc/SearchItem.java
@@ -1,7 +1,8 @@
 package rpc;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+
+import org.json.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,22 +30,24 @@ public class SearchItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("application/json");
 		response.addHeader("Access-Control-Allow-Origin", "*");
-
-		//Create a PrintWriter from response such that we can add data to response.
 		PrintWriter out = response.getWriter();
-		if (request.getParameter("username") != null) {
-			//Get the username sent from the client
-			String username = request.getParameter("username");
-            //In the output stream, add something to response body. 
-			out.print("Hello " + username);
+
+		JSONArray array = new JSONArray();
+		try {
+			array.put(new JSONObject().put("username", "abcd"));
+			array.put(new JSONObject().put("username", "1234"));
+		} catch (JSONException e) {
+			e.printStackTrace();
 		}
-		// Send response back to client
+		out.print(array);
 		out.flush();
 		out.close();
-
-
 	}
+
+
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
